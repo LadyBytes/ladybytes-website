19d94db91b83429379ba99a2cd571bd9703dd04d
Lisa Wagner <lisa.wagner@crealytics.com>
2018-07-19 15:16:09 -0400

    lets encrypt!

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,21 @@
-var routes = require('express').Router();
+var routes = require('express').Router()
 // var values = require('../values/values')
 
-var signup = require('./signup.js');
+var signup = require('./signup.js')
 
 // routes.use('/models', models);
-routes.use('/api/signup', signup);
+routes.use('/api/signup', signup)
 
 // app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }))
 
 routes.get('/api', (req, res) => {
-  res.status(200).json({ message: 'Connected!' });
-});
+  res.status(200).json({ message: 'Connected!' })
+})
+
+routes.get('/.well-known/acme-challenge/K_2RjEaeymUHc7iTWe-RdAFQjPLNdCdIuI1B2fnOpyk', (req, res) => {
+  res.send('K_2RjEaeymUHc7iTWe-RdAFQjPLNdCdIuI1B2fnOpyk.8QUstrwiUKpP7oZH0ycw6DC0Y1dZwoHI7v5syvojMU8')
+})
 
 // routes.get('/api/getValues', (req, res) => res.json(values))
 
-
-
-module.exports = routes;
+module.exports = routes

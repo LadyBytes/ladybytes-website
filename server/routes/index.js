d375c93cc41f79261445754bedf16c5cd99da019
Lisa Wagner <lisa.wagner@crealytics.com>
2018-07-19 15:34:25 -0400

    lets try again

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,6 +8,11 @@ routes.use('/api/signup', signup)
 
 // app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }))
 
+app.get('/.well-known/acme-challenge/x02bNvIcMOisnVArjLtb197MO6m7ZaCpvz0-Vt4xhHU', (req, res) => {
+  res.send('x02bNvIcMOisnVArjLtb197MO6m7ZaCpvz0-Vt4xhHU.cqg75Ou5dU5olf424JN0ifvSiEJ9hYJJBRPIidaWXOE')
+})
+
+
 routes.get('/api', (req, res) => {
   res.status(200).json({ message: 'Connected!' })
 })
